4d035ec88e05e6cb8627dc9ef3cacd8b9075b9d0
Jalendhar10 <56975828+Jalendhar10@users.noreply.github.com>
2023-08-18 10:48:24 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://nodebackend-api.onrender.com',
+    baseUrl: 'https://nodebackend-zm83.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
